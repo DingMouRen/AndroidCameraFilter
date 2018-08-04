c80d72a713c75c50209986b6ddcf911d6b7e4779
dingmouren <naildingmouren@gmail.com>
2018-08-04 09:49:38 +0800

    update

---
+++ camerafilter/src/main/java/com/dingmouren/camerafilter/FilterVideoActivity.java
@@ -0,0 +1,20 @@
+package com.dingmouren.camerafilter;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dingmouren
+ * email: naildingmouren@gmail.com
+ * github: https://github.com/DingMouRen
+ */
+
+public class FilterVideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter_video);
+    }
+}
